6f41420d1d98ae20cd1c960ea7694ef27c6b847a
jiangsk <jiangsk@inspur.com>
2019-12-04 17:48:33 +0800

    git

---
+++ src/main/java/com/github/resetpwd/CloudResetPwdAgent.java
@@ -0,0 +1,27 @@
+package com.github.resetpwd;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * @program: resetpwd
+ * @description:
+ * @author: jiangsk@inspur.com
+ * @create: 2019-12-04 17:40
+ **/
+public class CloudResetPwdAgent {
+    private static Logger logger = LogManager.getLogger(CloudResetPwdAgent.class);
+
+    public static void main(String[] args)
+    {
+        logger.info("CloudResetPwdAgent start..........");
+        ResetPwdFlow agent = new ResetPwdFlow();
+        try
+        {
+            agent.run();
+        }
+        catch (Exception e)
+        {
+            logger.error(e);
+        }
+        logger.info("CloudResetPwdAgent finish..........");
+    }
+}
