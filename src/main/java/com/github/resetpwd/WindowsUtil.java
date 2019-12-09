2d4a4b2dab14047d391d1960cdc705066039e70b
jiangsk <jiangsk@inspur.com>
2019-12-09 14:49:15 +0800

    git

---
+++ src/main/java/com/github/resetpwd/WindowsUtil.java
@@ -0,0 +1,19 @@
+package com.github.resetpwd;
+
+
+/**
+ * @program: resetpwd
+ * @description:
+ * @author: jiangsk@inspur.com
+ * @create: 2019-12-09 14:33
+ **/
+public class WindowsUtil {
+    //ShellExecute(0,"runas", LPCSTR("cmd.exe"),LPCSTR("/c net user administrator /active:yes"),"",SW_HIDE);
+//runas /noprofile /user:administrator\administrator "cmd /c \"net user administrator /active:yes\""
+    //ShellExecute(0,"runas","cmd","","",1);
+    public void setPasswd() {
+
+    }
+
+
+}
