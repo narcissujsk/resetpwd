6db1a6a355a5ffa11213416d549afc38b29d2f64
jiangsk <jiangsk@inspur.com>
2019-12-10 15:58:00 +0800

    git

--- src/main/java/com/github/resetpwd/AESUtil.java
+++ src/main/java/com/github/resetpwd/AESUtil.java
@@ -10,27 +10,24 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class AESUtil {
     private static Logger logger = Logger.getLogger(AESUtil.class);
-    /**
-     * 加密用的Key 可以用26个字母和数字组成
-     * 此处使用AES-128-CBC加密模式，key需要为16位。
-     */
+
     public String sKey = "1234567812345678";
     public String ivParameter = "1234567812345678";
 
-    // 加密
+    //
     public String encrypt(String sSrc) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         byte[] raw = getsKey().getBytes();
         SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-        IvParameterSpec iv = new IvParameterSpec(getIvParameter().getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
+        IvParameterSpec iv = new IvParameterSpec(getIvParameter().getBytes());//
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
         byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
         Base64.Encoder encoder = Base64.getEncoder();
-        byte[] str = encoder.encode(encrypted);//此处使用BASE64做转码。
+        byte[] str = encoder.encode(encrypted);//
         return new String(str, UTF_8);
     }
 
-    // 解密
+    //
     public String decrypt(String sSrc) {
         try {
             byte[] raw = getsKey().getBytes("ASCII");
@@ -39,7 +36,7 @@ public class AESUtil {
             IvParameterSpec iv = new IvParameterSpec(getIvParameter().getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
             Base64.Decoder decoder = Base64.getDecoder();
-            byte[] encrypted1 = decoder.decode(sSrc);//先用base64解密
+            byte[] encrypted1 = decoder.decode(sSrc);//
             byte[] original = cipher.doFinal(encrypted1);
             String originalString = new String(original, "utf-8");
             return originalString;
