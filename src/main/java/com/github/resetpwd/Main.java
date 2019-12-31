a23c0d55ff1e323e84031c02ab8931c58517ec4b
jiangsk <jiangsk@inspur.com>
2019-12-31 10:03:25 +0800

    git

--- src/main/java/com/github/resetpwd/Main.java
+++ src/main/java/com/github/resetpwd/Main.java
@@ -13,20 +13,20 @@ import java.util.Map;
  * @create: 2019-12-05 09:55
  **/
 public class Main {
-    private static Logger logger = Logger.getLogger(AESUtil.class);
+    private static Logger logger = Logger.getLogger(Main.class);
 
     public static void main(String[] args) throws UnsupportedEncodingException {
         logger.info("CloudResetPwdAgent start..........");
-        System.out.println(System.getProperty("file.encoding"));
-        logger.info(OSUtil.isWindows());
+        //System.out.println(System.getProperty("file.encoding"));
+        //logger.info(OSUtil.isWindows());
         try {
 
             while (true) {
                 Thread.sleep(3000);
                 // String re3 = WindowsUtil.executeCmd("net user administrator passwd123");
-                logger.info("agent run : " + new Date().toString());
-               // ResetPasswd.run();
-                logger.info("agent run : "+ new Date().toString());
+                //logger.info("agent run : " + new Date().toString());
+                ResetPasswd.run();
+                //logger.info("agent run : "+ new Date().toString());
             }
         } catch (Exception e) {
             logger.error(e);
